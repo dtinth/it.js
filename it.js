6dc38c39a75da6930dc2aa999ce38fac0f3922fc
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2013-07-05 20:23:29 +0700

    getting even more functional

--- it.js
+++ it.js
@@ -3,49 +3,100 @@
 
 var It = (function() {
 
-  var slice = [].slice,
-      own = {}.hasOwnProperty
 
+  var slice = [].slice,
+      own = {}.hasOwnProperty,
+      map = [].map
+
+
+  // the identity function
   function It(x) {
     return x
   }
 
-  function decorate(instance) {
-    for (var property in It.fn) {
-      if (own.call(It.fn, property)) {
-        instance[property] = It.fn[property]
+
+  // the context accessor function
+  It.self = function() {
+    return this
+  }
+
+
+  // utility functions
+  // -----------------
+
+  // extend: copy properties from src to dest, and return dest
+  function extend(dest, src) {
+    for (var property in src) {
+      if (own.call(src, property)) {
+        dest[property] = src[property]
       }
     }
-    return instance
+    return dest
   }
 
-  function compose(a, b) {
-    return function composedFunction(x) {
-      return a(b.call(this, x))
+  // rpartial: right-partial a function
+  function rpartial(fun, args) {
+    return function() {
+      return fun.apply(this, slice.call(arguments).concat(args))
     }
   }
 
-  It.fn = {
+  // compose: returns a function h(x) = g(f(x))
+  function compose(f, g) {
+    return function composedFunction(x) {
+      return g.call(this, f.call(this, x))
+    }
+  }
+
+  // create: create a new object with prototype
+  function create(prototype) {
+    var C = function() {}
+    C.prototype = prototype
+    return new C()
+  }
+
+
+  // functions framework
+  // -------------------
+  // we want to be as flexible as possible.
+  // it should be very easy to add more operations.
+
+  It.fn = { }
+  It.extend = function(functions) {
+    extend(It, functions)
+    extend(It.fn, functions)
+    extend(It.self, functions)
+  }
+
+
+  // functions
+  // ---------
+
+  // first, we want to be able to chain these things together
+  var decorate = rpartial(extend, [It.fn])
+
+  It.extend({
     compose: function(accessor) {
-      return decorate(compose(accessor, this))
-    },
-    get: function(property) {
-      return this.compose(function(object) {
-        return object[property]
-      })
-    },
-    set: function(property, value) {
-      return this.compose(function(object) {
-        object[property] = value
-        return object
-      })
-    },
-    del: function(property, value) {
-      return this.compose(function(object) {
-        delete object[property]
-        return object
-      })
-    },
+      return decorate(compose(this, accessor))
+    }
+  })
+
+  // operation: return a function that represent binary operation
+  function operation(func) {
+    return function binaryOperationFunction() {
+      return this.compose(rpartial(func, slice.call(arguments)))
+    }
+  }
+  
+  // primitive stuff: get set del
+  It.extend({
+    get: operation(function(it, property) { return it[property] }),
+    set: operation(function(it, property, value) { it[property] = value; return it }),
+    del: operation(function(it, property) { delete it[property]; return it }),
+  })
+
+  // function calling: post, send, fapply, fcall
+  It.extend({
     post: function(methodName, args) {
       var arg = args[0]
       if (typeof methodName == 'function') {
@@ -66,76 +117,41 @@ var It = (function() {
       var args = slice.call(arguments, 1)
       return this.post(methodName, args)
     },
-    fapply: function(args) {
-      return this.compose(function(fn) {
-        return fn.apply(null, args)
-      })
-    },
-    fcall: function() {
-      var args = slice.call(arguments)
-      return this.fapply(args)
-    },
-    or: function(defaultValue) {
-      return this.compose(function(value) {
-        return value || defaultValue
-      })
-    },
-    maybe: function(func) {
-      return this.compose(function(value) {
-        return value && func(value)
-      })
-    },
-    not: function(func) {
-      if (!func) func = It
-      return this.compose(function(value) {
-        return !func(value)
-      })
-    },
-    instantiate: function(Constructor) {
-      return this.compose(function(value) {
-        return new Constructor(value)
-      })
-    },
-    tap: function(func) {
-      return this.compose(function(value) {
-        func(value)
-        return value
-      })
-    },
-    splat: function(func) {
-      return this.compose(function(array) {
-        return Array.prototype.map.call(array, func)
-      })
-    },
-    pluck: function(property) {
-      return this.splat(It.get(property))
-    }
-  }
-
-  It.fn.put = It.fn.set
-  It.fn.invoke = It.fn.send
-
-  It.compare = function(accessor) {
-    function comparator(a, b) {
-      a = accessor(a)
-      b = accessor(b)
-      if (a < b) return -1
-      if (a > b) return 1
-      return 0
-    }
-    comparator.reverse = function(a, b) {
-      return -comparator(a, b)
-    }
-    return comparator
-  }
-
-  It.self = decorate(function() {
-    return this
+    fapply: operation(function(it, args) { return it.apply(null, args) }),
+    fcall: function() { return this.fapply(slice.call(arguments)) }
   })
 
-  It.decorate = decorate
 
-  return decorate(It)
+  // more awesome stuff: or, maybe, not
+  It.extend({
+    or: operation(function(it, defaultValue) { return it || defaultValue }),
+    maybe: operation(function(it, fun) { return it && fun(it) }),
+    instantiate: operation(function(it, Klass) { return new Klass(it) }),
+    tap: operation(function(it, fun) { fun(it); return it }),
+  })
+
+
+  // higher order function stuff, let's have a wrapper that defaults to It
+  function functional(op) {
+    return function(fun) {
+      return op.call(this, fun || It)
+    }
+  }
+
+  It.extend({
+    not: functional(operation(function(it, fun) { return !fun(it) })),
+    splat: functional(operation(function(it, fun) { return map.call(it, fun) })),
+    pluck: function(property) { return this.splat(It.get(property)) }
+  })
+
+
+  // aliases
+  It.extend({
+    put:     It.fn.set,
+    invoke:  It.fn.send
+  })
+
+  return It
 
 }())
 
